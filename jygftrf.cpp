052c064e457218f8c5cbef138b874fb62a5d46e5
RogelioCen <rogeliocentenosanchez@gmail.com>
2025-09-25 10:47:14 -0600

    Add files via upload

---
+++ jygftrf.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main (){
+	
+	/*float a = 0,b = 0,c = 0, resul = 0;
+	printf("Ingrese la primera calificacion: ");
+	scanf("%f", &a);
+	printf("Ingrese la segunda calificacion: ");
+	scanf("%f", &b);
+	printf("Ingrese la segunda calificacion: ");
+	scanf("%f", &c);
+	
+	resul = (a + b + c)/3;
+	
+	if (resul >= 7){
+		printf("Aprobado: %.2f", resul);
+	}else if (resul < 7){
+		printf("Reprobado: %.2f", resul);
+	}else {
+		printf("No valido");
+	}
+	return 0;*/
+	
+	/*int edad = 0;
+	printf("Ingrese su edad: ");
+	scanf("%d", &edad);
+	if (edad < 12){
+		printf("Es un chamaco");
+	}else if(edad >= 12 && edad <= 17){
+		printf("Es un adolescente");
+	}else if(edad >= 18 && edad <= 59){
+		printf("Es un adulto");
+	}else{
+		printf("Es un adulto mayor");
+	}*/
+	
+	/*int horas = 0, sueldo = 0, extras = 0;
+	printf("Ingrese el numero de horas trabajadas: ");
+	scanf("%d", &horas);
+	
+	if (horas <= 40){
+		sueldo = horas * 150;
+		printf("Su salario es de: %d", sueldo);
+	}else{
+		extras = horas - 40;
+		sueldo = ((extras * 150)*2) + ((horas - extras) * 150);
+		printf("%d", sueldo);
+	}*/
+	
+	
+}
